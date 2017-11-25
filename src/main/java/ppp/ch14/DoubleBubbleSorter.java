858a92f1c54728ab985a2df349c74d59c2afaa1f
KwonNam Son <kwon37xi@gmail.com>
2017-11-25 12:46:19 +0900

    bubble sorter template method pattern

---
+++ src/main/java/ppp/ch14/DoubleBubbleSorter.java
@@ -0,0 +1,31 @@
+package ppp.ch14;
+
+import java.util.Arrays;
+
+public class DoubleBubbleSorter extends BubbleSorter {
+    private double[] array = null;
+
+    public int sort(double[] theArray) {
+        array = theArray;
+        length = array.length;
+        return doSort();
+    }
+
+    @Override
+    protected void swap(int index) {
+        double temp = array[index];
+        array[index] = array[index + 1];
+        array[index + 1] = temp;
+    }
+
+    @Override
+    protected boolean outOfOrder(int index) {
+        return (array[index] > array[index + 1]);
+    }
+
+    public static void main(String[] args) {
+        double[] doubles = { 8.0, 2.0, 4.0, 6.0, 1.5, 3.3, 10.1, 7.7, 5.9, 9.2 };
+        (new DoubleBubbleSorter()).sort(doubles);
+        System.out.println("Soreted doubles : " + Arrays.toString(doubles));
+    }
+}
---
+++ src/main/java/ppp/ch14/IntBubbleSorter.java
@@ -0,0 +1,31 @@
+package ppp.ch14;
+
+import java.util.Arrays;
+
+public class IntBubbleSorter extends BubbleSorter {
+    private int[] array = null;
+
+    public int sort(int[] theArray) {
+        array = theArray;
+        length = array.length;
+        return doSort();
+    }
+
+    @Override
+    protected void swap(int index) {
+        int temp = array[index];
+        array[index] = array[index + 1];
+        array[index + 1] = temp;
+    }
+
+    @Override
+    protected boolean outOfOrder(int index) {
+        return (array[index] > array[index + 1]);
+    }
+
+    public static void main(String[] args) {
+        int[] ints = { 8, 2, 4, 6, 1, 3, 10, 7, 5, 9 };
+        (new IntBubbleSorter()).sort(ints);
+        System.out.println("Soreted ints : " + Arrays.toString(ints));
+    }
+}
